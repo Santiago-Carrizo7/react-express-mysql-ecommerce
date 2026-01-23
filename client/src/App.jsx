30c199b1e849de15664c0905dd51cdbff4ed644e
santi <santimanudecolon@gmail.com>
2026-01-22 22:25:06 -0300

    feat(client/router): implement protected routes and update navigation structure

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,10 +2,13 @@ import { Route, Routes } from "react-router-dom";
 import { useAuthStore } from "./store/AuthStore.jsx";
 import { useEffect } from "react";
 import { Header } from "./components/Header.jsx";
+import { ProtectedRoute } from "./components/ProtectedRoute.jsx";
 import { Home } from "./pages/Home.jsx";
 import { Cart } from "./pages/Cart.jsx";
 import { Login } from "./pages/Login.jsx";
+import { ProductsPage } from "./pages/ProductsPage.jsx";
 import { Register } from "./pages/Register.jsx";
+import { Footer } from "./components/Footer.jsx";
 
 export default function App() {
   const { checkAuth } = useAuthStore();
@@ -19,10 +22,14 @@ export default function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/cart" element={<Cart />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route element={<ProtectedRoute />}>
+          <Route path="/cart" element={<Cart />} />
+        </Route>
+        <Route path="/products" element={<ProductsPage />} />
       </Routes>
+      <Footer />
     </>
   );
 }
---
+++ client/src/components/ProtectedRoute.jsx
@@ -0,0 +1,13 @@
+import { Navigate, Outlet } from "react-router-dom";
+import { useAuthStore } from "../store/AuthStore.jsx";
+import { Spinner } from "../components/Spinner.jsx";
+
+export const ProtectedRoute = () => {
+  const { isAuthenticated, loading } = useAuthStore();
+
+  if (loading) return <div style={{padding: '50px', textAlign: 'center'}}><Spinner/></div>;
+  
+  if (!isAuthenticated) return <Navigate to="/login" replace />;
+
+  return <Outlet />;
+};
